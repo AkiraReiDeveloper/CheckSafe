a2f97c000fb9b028612cf8e7ddb794c8e280c7f8
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2019-04-19 11:54:09 -0600

    Interfaz inicial

---
+++ src/main/java/com/example/victorreyes/checksafe/CheckSafe.java
@@ -0,0 +1,13 @@
+package com.example.victorreyes.checksafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckSafe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_safe);
+    }
+}

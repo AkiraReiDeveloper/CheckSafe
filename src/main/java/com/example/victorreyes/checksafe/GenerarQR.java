931ac3c8c51a168d966f6da07af2de1632f41552
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2019-04-26 01:16:33 -0600

    Se agrego la clase GenerarQR que Genera un codigo QR a base del NoCuenta del usuario y lo almacena en el dispositivo, se adapto la interfaz del registro usuarios para agregar imagenes desde galeria.

---
+++ src/main/java/com/example/victorreyes/checksafe/GenerarQR.java
@@ -0,0 +1,132 @@
+package com.example.victorreyes.checksafe;
+
+import android.Manifest;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class GenerarQR {
+
+    public final static int QRcodeWidth = 500 ;
+    private String NameOfFile = "";
+    private String NameOfFolder = "";
+    private Context TheThis;
+
+    public void saveImage(Bitmap ImageToSave, Context context, String noCuenta, String nombre, String grupo, String grado) {
+
+        this.TheThis = context;
+        NameOfFolder = "/Gastronomia " + "Salon: " + grupo + "-" + grado;
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + NameOfFolder;
+        String CurrentDateAndTime = getCurrentDateAndTime();
+        File dir = new File(file_path);
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        //nombre de la imagen
+        NameOfFile = noCuenta + "_" + nombre + "_" + grupo + "_" + grado + "_";
+
+        File file = new File(dir, NameOfFile + CurrentDateAndTime + ".png");
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            ImageToSave.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+            MakeSureFileWasCreatedThenMakeAvabile(file);
+            AbleToSave();
+        }
+
+        catch(FileNotFoundException e) {
+            UnableToSave();
+        }
+        catch(IOException e) {
+            UnableToSave();
+        }
+
+    }
+
+    private void MakeSureFileWasCreatedThenMakeAvabile(File file){
+        MediaScannerConnection.scanFile(TheThis,
+                new String[] { file.toString() } , null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    public void onScanCompleted(String path, Uri uri) {
+                    }
+                });
+    }
+
+    private String getCurrentDateAndTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-­ss");
+        String formattedDate = df.format(c.getTime());
+        return formattedDate;
+    }
+
+    private void UnableToSave() {
+        Toast.makeText(TheThis, "¡No se ha podido guardar la imagen!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void AbleToSave() {
+        Toast.makeText(TheThis, "Imagen guardada en la galería.", Toast.LENGTH_SHORT).show();
+    }
+
+    public Bitmap TextToImageEncode(String Value, Context context) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        context.getResources().getColor(R.color.colorBlack):context.getResources().getColor(R.color.colorWhite);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+}
--- src/main/java/com/example/victorreyes/checksafe/RegistrarUsuario.java
+++ src/main/java/com/example/victorreyes/checksafe/RegistrarUsuario.java
@@ -1,12 +1,29 @@
 package com.example.victorreyes.checksafe;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -16,18 +33,51 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.zxing.WriterException;
 
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
 
 public class RegistrarUsuario extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     EditText campoId, campoNombre, campoApellido, campoEmail, campoGrado, campoGrupo;
     RadioButton campoFemenino, campoMasculino;
     String sexo = "";
-    //Button RegistButton;
+    Bitmap bitmap, bmap ;
+    ImageView imagen;
+    /*private static final String CARPETA_PRINCIPAL = "misImagenesApp/";//directorio principal
+    private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
+    private String mipath;//almacena la ruta de la imagen
+    File fileImagen;*/
+    public static final int COD_GALERIA = 10;
+    public static final int COD_FOTO = 20;
+
 
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
+
+    //permission STORAGE
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don’t have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }//fin permission STORAGE
 
 
     @Override
@@ -35,6 +85,8 @@ public class RegistrarUsuario extends AppCompatActivity implements Response.List
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_usuario);
+
+        verifyStoragePermissions(this);
 
         request = Volley.newRequestQueue(this);
 
@@ -46,21 +98,134 @@ public class RegistrarUsuario extends AppCompatActivity implements Response.List
         campoGrupo = (EditText) findViewById(R.id.txtGrupo);
         campoFemenino = (RadioButton) findViewById(R.id.radFemenino);
         campoMasculino = (RadioButton) findViewById(R.id.radMasculino);
+        imagen = (ImageView) findViewById(R.id.imgNewUser);
 
+    }
+
+    public void onClickimg(View view){
+
+        mostrarDialogOpciones();
+    }
+
+    private void mostrarDialogOpciones() {
+
+        final CharSequence[] opciones = { "Tomar Foto", "Elegir de Galeria", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Elige una Opción");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+                if(opciones[i].equals("Tomar Foto")){
+
+                    //abriCamara();
+                }else{
+                    if (opciones[i].equals("Elegir de Galeria")){
+
+                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicacion"),COD_GALERIA);
+                    }else{
+
+                        dialog.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+    /*private void abriCamara() {
+
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if(isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if(isCreada==true){
+            Long consecutivo= System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+
+            mipath=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;//indicamos la ruta de almacenamiento
+
+            fileImagen=new File(mipath);
+
+            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+
+            ////
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.N)
+            {
+                String authorities=this.getPackageName()+".provider";
+                Uri imageUri= FileProvider.getUriForFile(this,authorities,fileImagen);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            }else
+            {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fileImagen));
+            }
+            startActivityForResult(intent,COD_FOTO);
+
+            ////
+
+        }
+
+    }*/
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+
+            case COD_GALERIA:
+                Uri path = data.getData();
+                imagen.setImageURI(path);
+
+                try {
+                    bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),path);
+                    imagen.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            /*case COD_FOTO:
+                MediaScannerConnection.scanFile(this, new String[]{mipath}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String mipath, Uri uri) {
+                                Log.i("Path",""+ mipath);
+                            }
+                        });
+
+                bitmap= BitmapFactory.decodeFile(mipath);
+                imagen.setImageBitmap(bitmap);
+
+                break;*/
+        }
     }
 
     public void onClick(View view){
 
         cargarWebSevice();
-        if(campoFemenino.isChecked()){
+        GenerarCodigoQR();
+    }
 
-            sexo = "Femenino";
-        }else {
-            if(campoMasculino.isChecked()){
-
-                sexo = "Masculino";
+    private void GenerarCodigoQR() {
+        GenerarQR generarQR = new GenerarQR();
+        if(campoId.getText().toString()!=null){
+            try {
+                bitmap = generarQR.TextToImageEncode(campoId.getText().toString(), this);
+                generarQR.saveImage(bitmap, this, campoId.getText().toString(), campoNombre.getText().toString(), campoGrado.getText().toString(), campoGrupo.getText().toString() );  //give read write permission
+            } catch (WriterException e) {
+                e.printStackTrace();
             }
         }
+
     }
 
     private void cargarWebSevice() {

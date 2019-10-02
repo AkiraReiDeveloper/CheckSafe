4be8c496920c6870315e94c7995ac07031a1f25d
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2019-10-01 20:56:42 -0600

    modificacion de la ip

--- src/main/java/com/example/victorreyes/checksafe/Utilidades/VariablesEstaticas.java
+++ src/main/java/com/example/victorreyes/checksafe/Utilidades/VariablesEstaticas.java
@@ -1,5 +1,24 @@
 package com.example.victorreyes.checksafe.Utilidades;
 
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.victorreyes.checksafe.Adapters.UtileriaAdapter;
+import com.example.victorreyes.checksafe.Entidades.Utileria;
+import com.example.victorreyes.checksafe.Entidades.VolleySingleton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class VariablesEstaticas {
-    public static String ipAddress = "http://192.168.8.103";
+    public static String ipAddress = "http://192.168.3.111";
+    public String[] nombreUtilLista;
+    JsonObjectRequest stringRequest2;
+
 }
